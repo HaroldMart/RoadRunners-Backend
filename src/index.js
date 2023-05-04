156a4c38fa9a266ede225ab174c7f78684f6cca4
Haroldy <HaroldM09@outlook.com>
2023-05-04 11:03:03 -0400

    Arreglando cosas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 //requires
-require('dotenv').config();
+require('dotenv').config(); //A document .env in the project with the variable for connection
 const express = require('express');
 const morgan = require('morgan');
-const dotenv = require('dotenv').config();
-const path = require('path'); 
 const mongoose = require('mongoose');
 const vehiculesRoutes = require('./routes/vehicles')
 const URI = process.env.URI;
@@ -14,13 +12,6 @@ const port = 2000;
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(vehiculesRoutes);
-
-//mongoose connection
-mongoose.connect(URI).then(() => {
-    console.log('Conection succeded');
-}).catch((error) => {
-    console.log(`Conection failed: ${error}`);
-})
 
 //mongoose connection
 mongoose.connect(URI).then(() => {
